fbe76ffbc8fa1168ba93554892ee029c3f6e72ce
ptanhios <32252270+ptanhios@users.noreply.github.com>
2017-10-02 16:08:28 +0700

    Update CountryPickerViewController.swift
    searchController.searchBar.showCancelButton = false

--- Planet/CountryPickerViewController.swift
+++ Planet/CountryPickerViewController.swift
@@ -91,6 +91,7 @@ public extension CountryPickerViewController {
         searchController.searchResultsUpdater = self
         searchController.dimsBackgroundDuringPresentation = false
         searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.showCancelButton = false
         tableView.tableHeaderView = searchController.searchBar
         definesPresentationContext = true
         
